b433e7e36d97104ace4a454744c0ef6292a66dc7
Gsty <trroky070@gmail.com>
2026-09-14 10:33:03 +0300

    initial commit, setting up Spring Boot

---
+++ src/main/java/com/example/capstone1ecommercewebsite/Capstone1ECommerceWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.capstone1ecommercewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capstone1ECommerceWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Capstone1ECommerceWebsiteApplication.class, args);
+    }
+
+}
