f21d6388156f0290a3b230931165bab8ae9b52ec
Gsty <trroky070@gmail.com>
2026-09-15 14:18:03 +0300

    solved task 12

--- src/main/java/com/example/capstone1ecommercewebsite/Controller/MerchantController.java
+++ src/main/java/com/example/capstone1ecommercewebsite/Controller/MerchantController.java
@@ -57,15 +57,26 @@ public class MerchantController {
     public ResponseEntity<?> addStock(@PathVariable String productID, @PathVariable String merchantID, @PathVariable int stock) {
         int addingStatus = merchantService.addStock(productID, merchantID, stock);
 
-        if (addingStatus == 200)
-            return ResponseEntity.status(200).body(new ApiResponse("Stock increased by " + stock + " for product ID: " + productID));
+        return switch (addingStatus) {
+            case 200 -> ResponseEntity.status(200).body(new ApiResponse("Stock increased by " + stock + " for product ID: " + productID));
+            case 1 -> ResponseEntity.status(400).body(new ApiResponse("ProductID not found"));
+            case 2 -> ResponseEntity.status(400).body(new ApiResponse("Merchant ID not found"));
+            default -> ResponseEntity.status(400).body(new ApiResponse("IDs not added in the merchant stock yet, or merchant doesnt sell that product"));
+        };
+    }
 
-        if (addingStatus == 1)
-            return ResponseEntity.status(400).body(new ApiResponse("ProductID not found"));
+    @PutMapping("/buy/userid/{userID}/productid/{productID}/merchantid/{merchantID}")
+    public ResponseEntity<?> buyProduct(@PathVariable String userID, @PathVariable String productID, @PathVariable String merchantID) {
+        int buyingStatus = merchantService.buyProduct(userID, productID, merchantID);
 
-        if (addingStatus == 2)
-            return ResponseEntity.status(400).body(new ApiResponse("Merchant ID not found"));
-
-        return ResponseEntity.status(400).body(new ApiResponse("Not found in the merchant stock, or merchant doesnt sell that product"));
+        return switch (buyingStatus) {
+            case 200 -> ResponseEntity.status(200).body(new ApiResponse("Purchased successfully"));
+            case 1 -> ResponseEntity.status(400).body(new ApiResponse("User does not have enough balance to buy the product"));
+            case 2 -> ResponseEntity.status(400).body(new ApiResponse("User ID not found"));
+            case 3 -> ResponseEntity.status(400).body(new ApiResponse("Product ID not found"));
+            case 4 -> ResponseEntity.status(400).body(new ApiResponse("Merchant ID not found"));
+            case 5 -> ResponseEntity.status(400).body(new ApiResponse("Stock is empty"));
+            default -> ResponseEntity.status(400).body(new ApiResponse("Something went wrong"));
+        };
     }
 }
--- src/main/java/com/example/capstone1ecommercewebsite/Service/MerchantService.java
+++ src/main/java/com/example/capstone1ecommercewebsite/Service/MerchantService.java
@@ -3,6 +3,7 @@ package com.example.capstone1ecommercewebsite.Service;
 import com.example.capstone1ecommercewebsite.Model.Merchant;
 import com.example.capstone1ecommercewebsite.Model.MerchantStock;
 import com.example.capstone1ecommercewebsite.Model.Product;
+import com.example.capstone1ecommercewebsite.Model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,7 @@ public class MerchantService {
 
     private final ProductService productService;
     private final MerchantStockService merchantStockService;
+    private final UserService userService;
 
     private final ArrayList<Merchant> merchants = new ArrayList<>();
 
@@ -76,5 +78,69 @@ public class MerchantService {
             }
         }
         return 3; // 3 means that it didn't find either productID or merchantID in merchantStock
+    }
+
+    public int buyProduct(String userID, String productID, String merchantID) {
+
+        boolean correctUserID = false;
+        boolean correctProductID = false;
+        boolean correctMerchantID = false;
+        double userBalance = 0;
+        double productPrice = 0;
+        int userIndex = 0;
+
+
+        for (int i = 0; i < userService.getUsers().size(); i++) {
+            if (userService.getUsers().get(i).getId().equalsIgnoreCase(userID)) {
+                correctUserID = true;
+                userBalance = userService.getUsers().get(i).getBalance();
+                userIndex = i;
+                break;
+            }
+        }
+
+        for (Product p : productService.getProducts()) {
+            if (p.getId().equalsIgnoreCase(productID)) {
+                correctProductID = true;
+                productPrice = p.getPrice();
+                break;
+            }
+        }
+
+        if (productPrice > userBalance)
+            return 1; // means user does not have enough balance to buy this product
+
+        for (Merchant m : merchants) {
+            if (m.getId().equalsIgnoreCase(merchantID)) {
+                correctMerchantID = true;
+                break;
+            }
+        }
+
+        if (!correctUserID)
+            return 2; // means user ID not found
+
+        if (!correctProductID)
+            return 3; // means product ID not found
+
+        if (!correctMerchantID)
+            return 4; // means merchant ID not found
+
+        for (MerchantStock ms : merchantStockService.getMerchantStocks()) {
+            if (ms.getMerchantID().equalsIgnoreCase(merchantID) &&
+                    ms.getProductID().equalsIgnoreCase(productID) ) {
+
+                if (ms.getStock() < 1)
+                    return 5; // means stock is empty
+
+                ms.setStock(ms.getStock() - 1);
+
+                // deducting product price from suer balance
+                userService.getUsers().get(userIndex).setBalance(
+                    userService.getUsers().get(userIndex).getBalance() - productPrice);
+                return 200;
+            }
+        }
+        return 6; // means either merchant id or product not found in stock or the merchant do not sell that product
     }
 }
